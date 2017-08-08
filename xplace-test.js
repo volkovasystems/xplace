0f8d36d0c66c1aa1ccdfe462dc193bea8d5f9670
johnlenon <johnlenonmaghanoy@gmail.com>
2017-08-08 14:50:42 +0800

    Updated test.

--- xplace-test.js
+++ xplace-test.js
@@ -2,10 +2,10 @@
 const assert = require( "assert" );
 const xplace = require( "./xplace.js" );
 
-assert.deepEqual( xplace( { "hello": "world" } ), { "world": "hello" }, "should be deeply equal" );
+assert.deepEqual( xplace( { "hello": "world" } ), { "world": "hello" }, "should be equal to { 'world': 'hello' }" );
 
-assert.deepEqual( xplace( { "name": 1 } ), { 1: "name" }, "should be deeply equal" );
+assert.deepEqual( xplace( { "name": 1 } ), { 1: "name" }, "should be equal to { 1: 'name' }" );
 
-assert.deepEqual( xplace( { } ), { }, "should be deeply equal" );
+assert.deepEqual( xplace( { } ), { }, "should be equal to { }" );
 
 console.log( "ok" );
